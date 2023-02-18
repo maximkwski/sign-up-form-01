8f3c8c29c8074736844023f898267a6d6feca1d3
Maksym <makkie.makowski@gmail.com>
2023-02-18 14:23:42 -0500

    add styles to invalid inputs

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ const confirmPwd = document.querySelector('#confirm-pwd');
 const errorDiv = document.querySelector('.error')
 
 
+
+
+
+
 form.addEventListener('submit', (e) => {
     let errorMessage = [];
 
     if (pwd.value !== confirmPwd.value) {
         errorMessage.push('*Passwords do not match');
+        pwd.setAttribute('style', 'border:1px solid red');
+        confirmPwd.setAttribute('style', 'border:1px solid red');
     }
-    
     if (errorMessage.length > 0) {
         e.preventDefault()
         errorDiv.innerText = errorMessage.join(', ');
